5635e3532f96f671e37e0ac1122c5230dd228108
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-10-05 23:57:42 -0400

    updated index.js in models folder, will have to test to see if right format

--- models/index.js
+++ models/index.js
@@ -4,13 +4,19 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+//https://sequelize.org/docs/v6/advanced-association-concepts/polymorphic-associations/
+//https://sequelize.org/docs/v6/core-concepts/assocs/#ondelete-and-onupdate
 
+// Products belongsTo Category
+Products.belongsTo(Category);
 // Categories have many Products
+Category.hasMany(Products);
 
 // Products belongToMany Tags (through ProductTag)
+Products.belongsToMany(Tags, {through: ProductTag});
 
 // Tags belongToMany Products (through ProductTag)
+Tags.belongsToMany(Products, {through: ProductTag});
 
 module.exports = {
   Product,
