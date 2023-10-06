d0c5911552c2bdeb773966dbf2ab46b106c0dfb0
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-10-06 00:30:26 -0400

    added another option/format for association method in index.js based on hint given in challenge info

--- models/index.js
+++ models/index.js
@@ -13,11 +13,34 @@ Product.belongsTo(Category);
 // Categories have many Products
 Category.hasMany(Product);
 
+//option 1
 // Products belongToMany Tags (through ProductTag)
-Product.belongsToMany(Tag, {through: ProductTag});
+// Product.belongsToMany(Tag, {through: ProductTag});
 
 // Tags belongToMany Products (through ProductTag)
-Tag.belongsToMany(Product, {through: ProductTag});
+// Tag.belongsToMany(Product, {through: ProductTag});
+
+//option 2
+// Products belongsToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'product_id',
+  constraints: false
+});
+
+// Tags belongsToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'tag_id',
+  constraints: false
+});
+  
 
 module.exports = {
   Product,
