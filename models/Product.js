74d7fa7b75759ecd0eb2a05573a25ad9f7487907
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-10-05 23:40:51 -0400

    updated model columns for category, product, productTag, and tag.js

--- models/Product.js
+++ models/Product.js
@@ -10,7 +10,46 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+      },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      // Validates that the value is a decimal
+      //https://sequelize.org/docs/v6/core-concepts/validations-and-constraints/
+      validate: {
+        isDecimal: true,
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // Set a default value of 10
+      //https://sequelize.org/docs/v6/core-concepts/model-basics/#default-values
+      defaultValue: 10,
+      // Validates that the value is numeric
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      // References the category model's id
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
   },
+    
   {
     sequelize,
     timestamps: false,
