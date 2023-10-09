34b6ece38c78604a72432d251151064fa77bb2e9
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-10-08 23:55:58 -0400

    created delete route to delete category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -60,8 +60,18 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try { 
+    const deleteCategory = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    res.status(200).json(deleteCategory);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
