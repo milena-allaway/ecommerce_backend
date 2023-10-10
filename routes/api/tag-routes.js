868841826fb5303798cf0ae176b5b259395e84b9
Milena <132115087+milena-allaway@users.noreply.github.com>
2023-10-09 21:29:33 -0400

    created post route for tags to create new tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -33,8 +33,16 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create({
+      tag_name: req.body.tag_name,
+    });
+    res.status(200).json(newTag);
+    } catch (err) {
+    res.status(500).json(err);
+    }
 });
 
 router.put('/:id', (req, res) => {
